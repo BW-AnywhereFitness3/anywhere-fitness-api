15985667f941e9d202a437ecf2ba23af7566b9e9
Your Name <you@example.com>
2020-07-28 13:02:12 -0400

    attemp #2 fixing heroku connection

--- database/db-config.js
+++ database/db-config.js
@@ -1,8 +1,15 @@
-let knex = require("knex");
-let knexfile = require("../knexfile");
+// let knex = require("knex");
+// let knexfile = require("../knexfile");
 
-let environment = process.env.NODE_ENV || 'development';
+// let environment = process.env.NODE_ENV || 'development';
 
-// let configuration = knexfile.development;
+// // let configuration = knexfile.development;
 
-module.exports = knex(knexfile[environment]);
+// module.exports = knex(knexfile[environment]);
+
+const knex = require('knex');
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+
+module.exports = knex(config);
